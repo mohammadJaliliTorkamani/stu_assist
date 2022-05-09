7eeb087d3a8b850d341209f583c15d3528e61173
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-05-09 17:39:11 +0430

    initial modules created

---
+++ src/pages/ECTSCalculator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ECTSCalculator() {
+
+}
+
+
+export default ECTSCalculator
---
+++ src/pages/GPACalculator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function GPACalculator() {
+
+}
+
+
+export default GPACalculator
