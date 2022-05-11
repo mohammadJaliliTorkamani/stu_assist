e817255587b53d093a45ac417833b3d01b5b87f9
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-05-11 15:10:44 +0430

    Terms of use was created

--- src/pages/TermsOfUse.tsx
+++ src/pages/TermsOfUse.tsx
@@ -1,7 +1,93 @@
+import styled from "@emotion/styled";
 import React from "react";
+import Content from "../components/Content";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
-function TermsOfUse() {
+const footerPages = [{
+    id: 3,
+    text: "خانه",
+    link: "www.google.com"
+}, {
+    id: 2,
+    text: "ضوابط استفاده",
+    link: "www.facebook.com"
+}, {
+    id: 1,
+    text: "تماس با ما",
+    link: "www.space.com"
+}]
 
+const headerPages = [{
+    id: 3,
+    text: "خانه",
+    link: "www.google.com"
+}, {
+    id: 2,
+    text: "محاسبه GPA",
+    link: "www.facebook.com"
+}, {
+    id: 1,
+    text: "محاسبه ECTS",
+    link: "www.space.com"
+}]
+
+const contactLinks = [
+    {
+        id: 1,
+        label: "روابط عمومی",
+        value: "info@stu-assist.ir"
+    }, {
+        id: 2,
+        label: "ثبت بازخورد",
+        value: "customer@stu-assist.ir"
+    }
+]
+
+const Container = styled.div`
+    display: flex;
+    flex-direction : column;
+  `;
+
+const TextContiner = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10%;
+    padding-right: 10%;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+`;
+
+const TextBox = styled.div`
+    display: flex;
+    flex:1;
+    color: black;
+    background: #d1d1d1;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    text-align: justify;
+    border: 2px solid black;
+    border-radius: 1rem;
+    line-height: 2rem;
+`;
+
+interface IProps {
+    content: string
+}
+
+function TermsOfUse({ content }: IProps) {
+    return (
+        <Container className="App">
+            <Header pages={headerPages} user={"مهمان"} />
+            <TextContiner>
+                <TextBox>{content}</TextBox>
+            </TextContiner>
+            <Footer copyRightText={"تمامی حقوق مادی و معنوی محفوظ است - ۱۴۰۱"} pages={footerPages} contactUsLinks={contactLinks} />
+        </Container>
+    )
 }
 
 
