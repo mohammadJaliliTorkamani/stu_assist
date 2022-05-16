498b5bc58a9e743f35e1663fabc9becd07696d43
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-05-16 14:14:05 +0430

    multiple conditions were added to gpa calculation

--- src/pages/GPACalculator.tsx
+++ src/pages/GPACalculator.tsx
@@ -4,6 +4,7 @@ import Button from "../components/Button";
 import Footer from "../components/Footer";
 import TitledNumericInput from "../components/TitledNumericInput";
 import Header from "../components/Header";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const footerPages = [{
     id: 3,
@@ -70,6 +71,7 @@ const ResultContainer = styled.div`
     flex: 2;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
     padding-bottom: 3rem;
 `;
@@ -87,20 +89,78 @@ const ResultInnerContainer = styled.div`
 
 const Title = styled.div`
     font-size: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
 `;
 
 const Result = styled.div`
     font-size: 3.5rem;
 `;
 
+const LoginBox = styled.div`
+    display: flex;
+    flex-direction : row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ChargeBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ChargeOptions = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const ChargeOptionRecord = styled.div`
+    color: black;
+    cursor: pointer;
+    diosplay: flex;
+    flex-direction : row;
+    justify-content: cetner;
+    align-items: center;
+    margin: 1rem;
+    border: 1px solid orange;
+    border-radius: 4px;
+    padding: 1rem;
+    font-size: 0.85rem;
+`;
+
+const PayButton = styled.button`
+    color: white;
+    background: orange;
+    border-radius: 4px;
+    border: 0px solid orange;
+    width: 14rem;
+    height: 3rem;
+    margin-top: 1rem;
+    font-size : 0.94rem;
+    cursor: pointer;
+`;
+
+const chargeValue = [{ id: 1, value: 50, price: 50000 }, { id: 2, value: 200, price: 100000 }, { id: 3, value: 500, price: 250000 }, { id: 4, value: 1000, price: 500000 }]
+
 function GPACalculator() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [isGuest, setIsGuest] = useState(false)
+    const [isOutOfCoupon, setIsOutOfCoupon] = useState(true)
+    const [selectedChargeOption, setSelectedChargeOption] = useState({ id: -1, value: -1, price: -1 })
     const [min, setMin] = useState(0)
     const [max, setMax] = useState(0)
     const [grade, setGrade] = useState(0)
     const [gpa, setGPA] = useState(0)
 
+    const naviaget = useNavigate()
+
     const handleCalculate = () => {
-        setGPA(3 * ((max - grade) / (max - min)) + 1)
+        if (!isLoading && !isGuest)
+            setGPA(3 * ((max - grade) / (max - min)) + 1)
     }
 
     return (
@@ -114,12 +174,40 @@ function GPACalculator() {
                     <Button title={"محاسبه"} color={"orange"} onClick={() => handleCalculate()} />
                 </FieldsContainer>
                 <ResultContainer>
-                    <ResultInnerContainer>
-                        <Title>مقدار GPA (از 4.0) : </Title>
-                        <Result>
-                            {gpa}
-                        </Result>
-                    </ResultInnerContainer>
+                    {
+                        isLoading && <div>Loading</div>
+                    }
+                    {
+                        isGuest && !isLoading && <LoginBox>
+                            <Title>
+                                لطفا ابتدا وارد حساب کاربری خود شوید
+                            </Title>
+                            <Button color="orange" title="ورود / ثبت نام" onClick={e => naviaget('/login', { replace: true })} />
+                        </LoginBox>
+                    }
+                    {
+                        !isGuest && !isLoading && isOutOfCoupon &&
+                        <ChargeBox>
+                            <Title>تعداد کوپن های درخواست شما به پایان رسیده است</Title>
+                            <Title> {selectedChargeOption.id !== -1 ? `${selectedChargeOption.value} درخواست , ${selectedChargeOption.price} تومان` : "برای ادامه، لطفا یکی از گز ینه های پرداخت را انتخاب نمایید"} </Title>
+
+                            <ChargeOptions>
+                                {chargeValue.map(value => {
+                                    return <ChargeOptionRecord key={value.id} onClick={e => setSelectedChargeOption(value)}>{value.value} درخواست , {value.price} تومان</ChargeOptionRecord>
+                                })}
+                            </ChargeOptions>
+                            <PayButton>پرداخت</PayButton>
+                        </ChargeBox>
+                    }
+                    {
+                        !isLoading && !isGuest && !isOutOfCoupon &&
+                        <ResultInnerContainer>
+                            <Title>مقدار GPA (از 4.0) : </Title>
+                            <Result>
+                                {gpa}
+                            </Result>
+                        </ResultInnerContainer>
+                    }
                 </ResultContainer>
             </GPAContainer>
             <Footer copyRightText={"تمامی حقوق مادی و معنوی محفوظ است - ۱۴۰۱"} pages={footerPages} contactUsLinks={contactLinks} />
