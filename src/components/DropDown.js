adf8caf6f33fe25dc00142a9290040052bf6f6a3
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-19 15:27:33 +0430

    drop down react memo

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,16 +1,20 @@
 
 import MenuItem from "./MenuItem";
 import './DropDown.css'
-const Dropdown = ({ submenus, dropdown, depthLevel, onClick,externalLink }) => {
+import React from "react";
+
+const Dropdown = ({ submenus, dropdown, depthLevel, onClick, externalLink }) => {
+  console.log("S")
+
   depthLevel = depthLevel + 1;
   const dropdownClass = depthLevel > 1 ? "dropdown-submenu" : "";
   return (
     <ul className={`dropdown ${dropdownClass} ${dropdown ? "show" : ""}`} onClick={onClick}>
       {submenus.map((submenu, index) => (
-        <MenuItem items={submenu} key={index} depthLevel={depthLevel} externalLinks = {externalLink} />
+        <MenuItem items={submenu} key={index} depthLevel={depthLevel} externalLinks={externalLink} />
       ))}
     </ul>
   );
 };
 
-export default Dropdown;
+export default React.memo(Dropdown);
