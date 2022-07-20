82b8b99494c2cef9425ed2d903577a8e17296113
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-20 15:01:13 +0430

    URL creators became more general (localhost catcher)

--- src/utils/Utils.tsx
+++ src/utils/Utils.tsx
@@ -1,17 +1,17 @@
 import { BASE_URL } from "./Constants";
 
 export function createHallUrl(hallId: number) {
-    return BASE_URL + "forums/" + hallId
+    return (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : BASE_URL) + "forums/" + hallId
 }
 
 export function createTopicUrl(hallId: number, topicId: number) {
-    return BASE_URL + "forums/" + hallId + "/" + topicId
+    return (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : BASE_URL) + "forums/" + hallId + "/" + topicId
 }
 
 export function createProfileUrl(profileID: number) {
-    return BASE_URL + "profile/" + profileID
+    return (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : BASE_URL) + "profile/" + profileID
 }
 
 export function createBlogPostUrl(postID: number) {
-    return BASE_URL + "blogs/" + postID
+    return (process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : BASE_URL) + "blogs/" + postID
 }
