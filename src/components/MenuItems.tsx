a2f4dcd7e4ae19fcf3b4874a2086e143d162ea1a
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-22 20:54:55 +0430

    more embassies were added to header

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -32,9 +32,9 @@ export const MenuItems_Links = [
                 {
                     title: " PDE پست",
                     path: 'https://www.pdexp.com/',
-                },{
-                    title:"کارا پست",
-                    path:"https://karapost.com/",
+                }, {
+                    title: "کارا پست",
+                    path: "https://karapost.com/",
                 }
             ]
         }, {
@@ -43,7 +43,37 @@ export const MenuItems_Links = [
                 {
                     title: "سفارت آلمان",
                     path: 'https://teheran.diplo.de/',
-                }
+                }, {
+                    title: "سفارت اتریش",
+                    path: 'https://www.bmeia.gv.at/fa/oeb-teheran/',
+                }, {
+                    title: "سفارت استرالیا",
+                    path: 'http://www.iran.embassy.gov.au/',
+                }, {
+                    title: "سفارت انگلیس",
+                    path: 'https://www.gov.uk/world/organisations/british-embassy-tehran.fa',
+                }, {
+                    title: "سفارت ایتالیا",
+                    path: 'https://ambteheran.esteri.it/ambasciata_teheran/fa/',
+                }, {
+                    title: "سفارت سوئد",
+                    path: 'https://www.swedenabroad.se/en/embassies/iran-teheran/',
+                }, {
+                    title: "سفارت فرانسه",
+                    path: 'https://ir.ambafrance.org/-%D9%81%D8%A7%D8%B1%D8%B3%DB%8C-',
+                }, {
+                    title: "سفارت کانادا",
+                    path: 'https://www.canadainternational.gc.ca/iran/index.aspx?lang=eng',
+                }, {
+                    title: "سفارت فنلاند",
+                    path: 'https://finlandabroad.fi/web/irn/frontpage',
+                }, {
+                    title: "سفارت مالزی",
+                    path: 'http://www.kln.gov.my/web/irn_tehran/home',
+                }, {
+                    title: "سفارت نیوزلند",
+                    path: 'https://www.mfat.govt.nz/en/',
+                },
             ]
         }, {
             title: 'ویزامتریک آلمان',
