51e85ad8487f298ad06620dd48f7a41d0fc5f37e
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-28 22:27:11 +0430

    samandehi resized

--- src/components/tsx/Footer.tsx
+++ src/components/tsx/Footer.tsx
@@ -26,7 +26,7 @@ function Footer() {
             <img
                 referrerPolicy='origin'
                 id='rgvjjxlzapfujzpergvjesgt'
-                style={{ cursor: 'pointer' }}
+                style={{ cursor: 'pointer', height:'60px', width:'60px' }}
                 onClick={e => window.open("https://logo.samandehi.ir/Verify.aspx?id=315730&p=xlaorfthdshwjyoexlaoobpd", "Popup", "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")}
                 alt='logo-samandehi'
                 src='https://logo.samandehi.ir/logo.aspx?id=315730&p=qftinbpdujynyndtqftilyma' />
