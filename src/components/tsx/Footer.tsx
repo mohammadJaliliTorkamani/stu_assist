272ad5faeafeec8b518e24d7b50b4a769e2bc4ec
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-28 22:18:08 +0430

    samandehi logo was added to get the certificate

--- src/components/tsx/Footer.tsx
+++ src/components/tsx/Footer.tsx
@@ -4,11 +4,13 @@ import React from "react"
 
 const FooterContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column
     color: white;
     height: 80px;
     width: 100%;
-    justify-content : center;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    justify-content : space-between;
     align-items: center;
     background: black;
     direction: rtl;
@@ -18,7 +20,16 @@ function Footer() {
 
     return (
         <FooterContainer>
-            {FOOTER_TEXT}
+            <div style={{ color: 'white', fontSize: '0.9em' }}>
+                {FOOTER_TEXT}
+            </div>
+            <img
+                referrerPolicy='origin'
+                id='rgvjjxlzapfujzpergvjesgt'
+                style={{ cursor: 'pointer' }}
+                onClick={e => window.open("https://logo.samandehi.ir/Verify.aspx?id=315730&p=xlaorfthdshwjyoexlaoobpd", "Popup", "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")}
+                alt='logo-samandehi'
+                src='https://logo.samandehi.ir/logo.aspx?id=315730&p=qftinbpdujynyndtqftilyma' />
         </FooterContainer>
     )
 }
