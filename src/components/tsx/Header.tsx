a18b9ce5617b09858c49ee2b7c69bd78966142a8
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-29 17:03:27 +0430

    header style got revisioned for laptop (mobile remained)

--- src/components/tsx/Header.tsx
+++ src/components/tsx/Header.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocalStorage } from "../../utils/useLocalStorage";
-import { MenuItems, MenuItems_Links } from "./MenuItems";
+import { MenuItems } from "./MenuItems";
 import '../css/Header.css'
 
 import React from 'react'
@@ -17,55 +17,54 @@ function Header() {
 
     return (
         <div className="nav-bar">
-            <Link className="navbar-logo-container" onClick={e => setIsMobileMenuOpen(false)} to="/">
-                <div className="navbar-logo-text">Stu-Assist</div>
-                <div className="navbar-logo-icon" />
+            <Link className="profile-button" to={!isUser ? 'login' : 'profile'} style={{ textDecoration: 'none' }}>
+                <Button title={!isUser ? "ورود / ثبت نام" : "حساب کاربری"} onClick={e => null} />
             </Link>
             <div className='menu-icon' onClick={e => setIsMobileMenuOpen(!isMobileMenuOpen)}>
                 <i className={isMobileMenuOpen ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
-            <ul className={isMobileMenuOpen ? 'nav-menu active' : 'nav-menu'}>
-                {isUser && <li className='nav-item'>
-                    <div className='nav-links logout' onClick={e => {
-                        setToken(null)
-                        setIsMobileMenuOpen(false)
-                        window.location.reload()
-                    }}>
-                        خروج از حساب کاربری
-                    </div>
-                </li>
-                }
-                <li className='nav-item'>
-                    <Link to='/about-us' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
-                        درباره ما
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/blogs' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
-                        وبلاگ
-                    </Link>
-                </li>
-                <li className='nav-item '>
-                    {
-                        MenuItems.map((item, index) => {
-                            return <MenuItem items={item} key={index} depthLevel={0} externalLinks={false} onClick={() => setIsMobileMenuOpen(false)} />;
-                        })
+            <div className="header-options-container">
+                <ul className={isMobileMenuOpen ? 'nav-menu active' : 'nav-menu'}>
+                    {isUser && <li className='nav-item'>
+                        <div className='nav-links logout' onClick={e => {
+                            setToken(null)
+                            setIsMobileMenuOpen(false)
+                            window.location.reload()
+                        }}>
+                            خروج از حساب کاربری
+                        </div>
+                    </li>
                     }
-                </li>
-                <li>
-                    <Link to={!isUser ? 'login' : 'profile'} className='nav-links-mobile' onClick={e => setIsMobileMenuOpen(false)}>
-                        {!isUser ? "ورود / ثبت نام" : "حساب کاربری"}
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
-                        خانه
-                    </Link>
-                </li>
-            </ul>
-            <Link className="profile-button" to={!isUser ? 'login' : 'profile'} style={{ textDecoration: 'none' }}>
-                <Button title={!isUser ? "ورود / ثبت نام" : "حساب کاربری"} onClick={e => null} />
-            </Link>
+                    <li className='nav-item'>
+                        <Link to='/about-us' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
+                            درباره ما
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/blogs' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
+                            وبلاگ
+                        </Link>
+                    </li>
+                    <li className='nav-item '>
+                        {
+                            MenuItems.map((item, index) => {
+                                return <MenuItem items={item} key={index} depthLevel={0} externalLinks={false} onClick={() => setIsMobileMenuOpen(false)} />;
+                            })
+                        }
+                    </li>
+                    <li>
+                        <Link to={!isUser ? 'login' : 'profile'} className='nav-links-mobile' onClick={e => setIsMobileMenuOpen(false)}>
+                            {!isUser ? "ورود / ثبت نام" : "حساب کاربری"}
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={e => setIsMobileMenuOpen(false)}>
+                            خانه
+                        </Link>
+                    </li>
+                </ul>
+                <div className="navbar-logo-icon" />
+            </div>
         </div>
     );
 }
--- src/components/tsx/MenuItems.tsx
+++ src/components/tsx/MenuItems.tsx
@@ -2,89 +2,67 @@ export const MenuItems = [
     {
         title: 'خدمات',
         submenu: [{
-            title: 'GPA محاسبه',
-            path: '/gpa-calculator',
-        }, {
-            title: 'ECTS محاسبه',
-            path: '/ects-calculator',
-        }, {
-            title: 'تجربه پذیرش',
-            path: '/application-experience',
-        }, {
-            title: 'تالار گفتگو',
-            path: '/forums',
-        }, {
-            title: 'دارالترجمه های رسمی',
-            path: '/translation-offices',
-        }, {
-            title: 'پیوند ها',
-            submenu: [{
-                title: 'امور تحصیلی',
-                submenu: [
-                    {
-                        title: "سامانه سجاد",
-                        path: 'https://portal.saorg.ir/',
-                    },
-                    {
-                        title: "سامانه تاییدیه تحصیلی",
-                        path: "https://emt.medu.ir/",
-                    }
-                ]
-            }, {
-                title: 'پست بین المللی',
-                submenu: [
-                    {
-                        title: " PDE پست",
-                        path: 'https://www.pdexp.com/',
-                    }, {
-                        title: "کارا پست",
-                        path: "https://karapost.com/",
-                    }
-                ]
-            }, {
-                title: 'سفارت ها',
-                submenu: []
-            }, {
-                title: 'ویزامتریک آلمان',
-                path: 'https://www.visametric.com/iran/Germany/fa',
-            }]
-        }]
-    },
-];
-
-export const MenuItems_Links = [
-    {
-        title: 'پیوند ها',
-        submenu: [{
-            title: 'امور تحصیلی',
+            title: 'پذیرش',
             submenu: [
                 {
-                    title: "سامانه سجاد",
-                    path: 'https://portal.saorg.ir/',
-                },
-                {
-                    title: "سامانه تاییدیه تحصیلی",
-                    path: "https://emt.medu.ir/",
-                }
-            ]
-        }, {
-            title: 'پست بین المللی',
-            submenu: [
-                {
-                    title: " PDE پست",
-                    path: 'https://www.pdexp.com/',
+                    title: 'تجربه پذیرش',
+                    path: '/application-experience',
                 }, {
-                    title: "کارا پست",
-                    path: "https://karapost.com/",
+                    title: 'GPA محاسبه',
+                    path: '/gpa-calculator',
+                }, {
+                    title: 'ECTS محاسبه',
+                    path: '/ects-calculator',
                 }
             ]
         }, {
-            title: 'سفارت ها',
-            submenu: []
+            title: 'ویزا',
+            submenu: [
+                {
+                    title: 'دارالترجمه های رسمی',
+                    path: '/translation-offices',
+                }
+            ]
         }, {
-            title: 'ویزامتریک آلمان',
-            path: 'https://www.visametric.com/iran/Germany/fa',
+            title: 'عمومی',
+            submenu: [
+                {
+                    title: 'تالار گفتگو',
+                    path: '/forums',
+                }, {
+                    title: 'منابع',
+                    submenu: [{
+                        title: 'امور تحصیلی',
+                        submenu: [
+                            {
+                                title: "سامانه سجاد",
+                                path: 'https://portal.saorg.ir/',
+                            },
+                            {
+                                title: "سامانه تاییدیه تحصیلی",
+                                path: "https://emt.medu.ir/",
+                            }
+                        ]
+                    }, {
+                        title: 'پست بین المللی',
+                        submenu: [
+                            {
+                                title: " PDE پست",
+                                path: 'https://www.pdexp.com/',
+                            }, {
+                                title: "کارا پست",
+                                path: "https://karapost.com/",
+                            }
+                        ]
+                    }, {
+                        title: 'سفارت ها',
+                        submenu: []
+                    }, {
+                        title: 'ویزامتریک آلمان',
+                        path: 'https://www.visametric.com/iran/Germany/fa',
+                    }]
+                }
+            ]
         }]
     },
 ];
-
