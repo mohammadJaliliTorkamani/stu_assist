266e06a1fa96f7875b9a406157a69a91c8d24600
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-29 17:38:49 +0430

    footer changed

--- src/components/tsx/Footer.tsx
+++ src/components/tsx/Footer.tsx
@@ -1,36 +1,24 @@
-import styled from "@emotion/styled"
 import { FOOTER_TEXT } from "../../utils/Constants"
 import React from "react"
+import '../css/Footer.css'
 
-const FooterContainer = styled.div`
-    display: flex;
-    flex-direction: column
-    color: white;
-    height: 80px;
-    width: 100%;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    justify-content : space-between;
-    align-items: center;
-    background: black;
-    direction: rtl;
-`
 
 function Footer() {
 
     return (
-        <FooterContainer>
+        <div className="footer-container">
+            <div></div>
             <div style={{ color: 'white', fontSize: '0.9em' }}>
                 {FOOTER_TEXT}
             </div>
             <img
                 referrerPolicy='origin'
                 id='rgvjjxlzapfujzpergvjesgt'
-                style={{ cursor: 'pointer', height:'60px', width:'60px' }}
+                className='footer-samandehi'
                 onClick={e => window.open("https://logo.samandehi.ir/Verify.aspx?id=315730&p=xlaorfthdshwjyoexlaoobpd", "Popup", "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")}
                 alt='logo-samandehi'
                 src='https://logo.samandehi.ir/logo.aspx?id=315730&p=qftinbpdujynyndtqftilyma' />
-        </FooterContainer>
+        </div>
     )
 }
 
